81d118e1b827344aa5e3d2792729e79b873c16c3
NishPatel1 <97016195+NishPatel1@users.noreply.github.com>
2022-09-30 23:03:21 +0530

    Delete TestThreadPrc1b.java

--- Practical1/TestThreadPrc1b.java
+++
@@ -1,7 +0,0 @@
-public class TestThreadPrc1b {
-    public static void main(String[] args) {
-        ThreadExaPrac1b b = new ThreadExaPrac1b();
-        Thread t = new Thread(b);
-        t.start();
-    }
-}
